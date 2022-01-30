ee48f433be37d414b304a6c0acaf42f5967997b6
Alex <alexeytitovwork@gmail.com>
2022-01-31 01:22:41 +0300

    Update

--- cuda_test.cu
+++ cuda_test.cu
@@ -1,6 +1,6 @@
 // coded by Alex. 28.10.2021
 // alexeytitovwork@gmail.com
-// Torch to CUDA data transfer test.
+// Bit finder
 #include <torch/script.h>
 #include <torch/torch.h>
 #include <memory>
@@ -39,29 +39,6 @@ time ./cuda_test
   Processing result:
   root@3ec54f35ea02:/home/loader_torch_init_step# cmake --build . --config Release
 
-Sun Jan 16 14:58:54 2022       
-+-----------------------------------------------------------------------------+
-| NVIDIA-SMI 460.91.03    Driver Version: 460.91.03    CUDA Version: 11.2     |
-|-------------------------------+----------------------+----------------------+
-| GPU  Name        Persistence-M| Bus-Id        Disp.A | Volatile Uncorr. ECC |
-| Fan  Temp  Perf  Pwr:Usage/Cap|         Memory-Usage | GPU-Util  Compute M. |
-|                               |                      |               MIG M. |
-|===============================+======================+======================|
-|   0  GeForce RTX 208...  Off  | 00000000:01:00.0 Off |                  N/A |
-| 25%   42C    P2    53W / 257W |    988MiB / 11016MiB |     10%      Default |
-|                               |                      |                  N/A |
-+-------------------------------+----------------------+----------------------+
-|   1  GeForce RTX 208...  Off  | 00000000:02:00.0 Off |                  N/A |
-|  7%   44C    P0    62W / 250W |      3MiB / 11019MiB |      1%      Default |
-|                               |                      |                  N/A |
-+-------------------------------+----------------------+----------------------+                                                                               
-+-----------------------------------------------------------------------------+
-| Processes:                                                                  |
-|  GPU   GI   CI        PID   Type   Process name                  GPU Memory |
-|        ID   ID                                                   Usage      |
-|=============================================================================|
-|    0   N/A  N/A    944573      C   ./loader_torch_init_step          985MiB |
-+-----------------------------------------------------------------------------+
 */
 //------------------------------------------------------------------------------
 __global__
